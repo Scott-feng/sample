3bc33a3127da5758117f7a8eeed4e9ca87966141
vagrant <vagrant@homestead>
2018-07-14 06:13:51 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sample App</title>
+	<title>@yield('title','Sample')</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
